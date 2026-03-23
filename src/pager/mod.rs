31465d3923396a1534e60aa6750d5a4375e8ba51
Rani Elhusseini <charmful0x@gmail.com>
2026-03-23 12:23:09 +0100

    chore: switch assignment height to inspector href

--- src/pager/mod.rs
+++ src/pager/mod.rs
@@ -5,3 +5,4 @@ pub mod state;
 pub const START_BLOCK: u64 = 1_881_888;
 pub const LUNAR_EXPLORER_BASE_URL: &str = "https://lunar.ar.io/#/explorer";
 pub const VIEWBLOCK_BLOCK_BASE_URL: &str = "https://viewblock.io/arweave/block";
+pub const INSPECTOR_BLOCK_BASE_URL: &str = "https://ao-inspector.decent.land/v1/token/ao/transfers";
